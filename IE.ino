b277b5d3dcc119661dd105a86d0541c47607c825
CESARARGUELLO <62823939+CESARARGUELLO@users.noreply.github.com>
2020-03-30 13:40:30 -0500

    Add files via upload

---
+++ IE.ino
@@ -0,0 +1,37 @@
+
+const int Pinbombain =  13;
+const int Pinbombaout = 12;
+int t          = 0;
+int valorPotenciometro = 0;  // variable para guardar el valor leido del sensor
+  const int pinSensor = 0;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(Pinbombain, OUTPUT);
+  pinMode(Pinbombaout, OUTPUT);
+   pinMode(pinSensor, INPUT);
+   Serial.begin(9600);    
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  valorPotenciometro = analogRead(pinSensor);
+  t= map(valorPotenciometro, 0, 1023, 1500, 3000  );
+  digitalWrite(Pinbombain, HIGH);   // turn the LED on (HIGH is the voltage level) 
+  
+  delay(1000);                       // wait for a second
+  digitalWrite(Pinbombain, LOW);    // turn the LED off by making the voltage LOW
+  delay(200+t);
+                    // wait for a second
+
+
+                   Serial.print("Valor del Potenciometro = ");
+                   Serial.print(valorPotenciometro);   
+                   Serial.print("    t = ");  
+
+                   Serial.println(t);   
+ 
+                   delay(100);
+                    
+  
+}
