ab590d1318aecf1586c74c58cec8099965dd487a
CESARARGUELLO <62823939+CESARARGUELLO@users.noreply.github.com>
2020-03-30 11:39:20 -0500

    Add files via upload

---
+++ comparador_flujo.ino
@@ -0,0 +1,43 @@
+volatile int NbTopsFan; 
+int Calc;
+int suma;
+int rele = 4;
+int parametro;
+int valvula  = 3;
+int hallsensor = 2; 
+void rpm () 
+{
+NbTopsFan++; 
+}
+void setup() //
+{ 
+pinMode(hallsensor, INPUT);
+pinMode(rele, OUTPUT);
+pinMode(valvula, OUTPUT); 
+Serial.begin(9600); 
+attachInterrupt(0, rpm, RISING); 
+}
+void loop ()
+{
+  digitalWrite(rele, HIGH);
+NbTopsFan = 0;
+
+sei(); 
+delay (100); 
+cli(); 
+Calc = (NbTopsFan * 5.5);
+
+suma= suma+Calc;
+if (suma <= 60){
+  digitalWrite(valvula, LOW);
+
+
+   
+}else{
+  digitalWrite(valvula, HIGH );
+  suma=0;
+}
+Serial.print (suma, DEC); 
+Serial.print (" L/min \r\n"); 
+
+}
